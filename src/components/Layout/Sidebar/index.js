9b73a672352448e6628e7af361babac64599080b
Mack <mldixon9750@gmail.com>
2024-02-21 01:32:09 -0800

    added some styling/nav links to sidebar

--- src/components/Layout/Sidebar/index.js
+++ src/components/Layout/Sidebar/index.js
@@ -1,14 +1,30 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
 import LogoS from '../../../assets/images/logo-m.png'
 import LogoSubtitle from '../../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Sidebar = () => (
     <div className='nav-bar'>
         <Link className='logo' to='/'>
             <img src={LogoS} alt='logo' />
-            <img src={LogoSubtitle} alt='mack' />
+            <img className="sub-logo" src={LogoSubtitle} alt='mack' />
         </Link>
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#dbce0e" />
+
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" to="/about">
+                <FontAwesomeIcon icon={faUser} className="about-link" color="#dbce0e" />
+
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} className="contact-link" color="#dbce0e" />
+
+            </NavLink>
+        </nav>
     </div>
 )
 
